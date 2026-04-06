3665dd2f3c1484c8b211f8b0da543e69a35165cb
Zara Danziger <zara.danziger15@gmail.com>
2026-04-06 16:11:17 +1000

    added error handeling

--- 5.2p/server.js
+++ 5.2p/server.js
@@ -15,6 +15,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.use((req, res) => res.status(404).json({ message: 'Not found' }));
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: 'Server error' });
 });
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
